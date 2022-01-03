8db92582b73383d049a426c20044c52356582a6e
tarekkhatib <tarekkhatib@localhost>
2022-01-03 14:09:13 -0300

    Commit v2

--- TheatreProject/src/controller/InputEngine.java
+++ TheatreProject/src/controller/InputEngine.java
@@ -1,5 +1,54 @@
 package controller;
 
-public class InputEngine {
+import util.InputReader;
+import model.Transaction;
+import model.SQLConverter;
 
+public class InputEngine {
+	
+	/*
+	 * Instance variables.
+	 */
+	private InputReader reader;
+	private Transaction order;
+	private SQLConverter sql;
+	
+	/*
+	 * Constructor where an instance of the InputReader class is
+	 * created amongst other things. 
+	 */
+	public InputEngine() {
+		reader = new InputReader();
+		order = new Transaction();
+		sql = new SQLConverter();
+	}
+	
+	/*
+	 * Method to add tickets to the transaction basket. 
+	 */
+	public void addTickets() {
+		
+	}
+	
+	/*
+	 * Method to get finalised order. 
+	 */
+	public void getOrder() {
+		
+	}
+	
+	/*
+	 * Method to finalise order in basket (i.e. check-out).
+	 */
+	public void finaliseOrder() {
+		
+	}
+	
+	/*
+	 * Method to get contents of the basket.
+	 */
+	public void getBasket() {
+		
+	}
+	
 }
--- TheatreProject/src/model/Transaction.java
+++ TheatreProject/src/model/Transaction.java
@@ -1,5 +1,37 @@
 package model;
 
-public class Transaction {
+import java.util.*;
 
+public class Transaction {
+	
+	/*
+	 * Instance variables including a 3D ArrayList for 
+	 * storing tickets.
+	 */
+	private ArrayList<ArrayList<ArrayList<Integer>>> tickets;
+
+	/*
+	 * Constructor that initialises each element of 
+	 * 3D ArrayList 'tickets'.
+	 */
+	public Transaction() {
+		tickets = new ArrayList();
+		for (int)
+	}
+	
+	/*
+	 * Method to return 3D ArrayList tickets.
+	 */
+	public ArrayList<ArrayList<ArrayList<Integer>>> getTickets() {
+		return tickets;
+	}
+	
+	/*
+	 * Method to add ticket information to 3D ArrayList
+	 * 'tickets'.
+	 */
+	public void addTickets(int x, int y, int z) {
+		
+	}
+	
 }
