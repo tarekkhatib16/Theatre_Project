32a5718756aefb5890aa5738361400799b3e10a3
tarekkhatib <tarekkhatib@localhost>
2022-01-03 13:26:11 -0300

    # WARNING: head commit changed in the meantime
    InputReader V1

--- TheatreProject/src/util/InputReader.java
+++ TheatreProject/src/util/InputReader.java
@@ -1,5 +1,18 @@
 package util;
 
-public class InputReader {
+import java.util.Scanner;
 
+public class InputReader {
+	
+	private Scanner reader;
+
+	public InputReader() {
+		reader = new Scanner(System.in);
+	}
+	
+	public String getInput() {
+		String inputLine = reader.nextLine();
+		return inputLine;
+	}
+	
 }
