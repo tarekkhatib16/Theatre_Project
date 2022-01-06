6fd8e7d23ee3fdf69af7c1252a4055373b283a99
StuMccly <stuartmcclymont@live.com>
2022-01-06 00:22:16 +0000

    V2_3
    CreateProcedures: added Procedure to BOOK a ticket:-  SetBooking(IN PerfID INT, IN PurID INT, IN Conc BOOL, IN Stalls BOOL, OUT RetValue BOOL)
     -- Need to impliment a return TRUE if successful and ROLLBACK checks
    
    Test_Procedures: added tests for SetBooking() Procedure

--- SQL/CreateProcedures.sql
+++ SQL/CreateProcedures.sql
@@ -1,6 +1,6 @@
 /*
 
-VERSION: SM_V2_2
+VERSION: SM_V2_3 : added booking Procedure
 */
 
 /*
@@ -46,4 +46,36 @@ CREATE PROCEDURE GetShowsDate(IN SearchQ DATE)
         ON EventInfo.EventID = Performances.EventID
         WHERE PerformanceStart LIKE CONCAT(SearchQ,'%'); -- Query must be in format YYYY-MM-DD STRING
 
+	END; //
+    
+    
+/*
+Procedure to add a new line to bookings table for ticket purchases
+Stalls BOOL True if seat in stalls, FALSE if seat in Circle (log price at time of purchase in case of refund/pricechange
+*/
+DROP PROCEDURE IF EXISTS finalprojecttheatre.SetBooking//
+CREATE PROCEDURE SetBooking(IN PerfID INT, IN PurID INT, IN Conc BOOL, IN Stalls BOOL, OUT RetValue BOOL)
+	BEGIN
+		/*Subtract a seat from available seats*/ -- *? Check table that seats are available
+		UPDATE Performances
+        SET AvailabilityOfTickets = AvailabilityOfTickets - 1
+        WHERE Performances.PerformanceID = PerfID;
+        
+        /*get the price of the ticket*/
+         SET @Price = IF (Stalls, 
+			((SELECT PricePenceStall FROM EventInfo -- Stall Seat
+				WHERE EventInfo.EventID = 
+					(SELECT EventID FROM Performances 
+                    WHERE Performances.PerformanceID = PerfID))),
+                    
+			(SELECT PricePenceCircle FROM EventInfo -- Circle Seat
+				WHERE EventInfo.EventID = 
+					(SELECT EventID FROM Performances 
+                    WHERE Performances.PerformanceID = PerfID)));
+        
+        /*Add the values to bookings table*/
+		INSERT INTO Bookings(PerformanceID, PurchaserID, Concessionary,TicketPricePence)
+        VALUES(PerfID, PurID, Conc, @Price);
+        
+        /*Return TRUE if Successful*/
 	END; //
--- SQL/Test_Procedures.sql
+++ SQL/Test_Procedures.sql
@@ -1,20 +1,29 @@
 /*
 Test Stored Procedures
 
-VERSION: SM_V2_0
+VERSION: SM_V2_3: added tes for booking procedure (populate tables before running)
 */
 
 /*
 Test Return list of all Events from EventInfo
 */
-CALL GetShows();
+-- CALL GetShows();
 
 /*
 Test Return list of all Events from EventInfo with Search term in Title
 */
-CALL GetShowsSearch('a');
+-- CALL GetShowsSearch('a');
 
 /*
 Test Return list of all Events with search term dates
 */
-CALL GetShowsDate('2022-06-01');
+-- CALL GetShowsDate('2022-06-01');
+
+/*
+Test Book a ticket to event, should add ticket to Bookings, Reduce available seats from performance, log ticketprice and concession
+*/
+-- CALL SetBooking(1, 1, FALSE, TRUE, @Ret); -- Stall Seat
+-- CALL SetBooking(1, 1, FALSE, FALSE, @Ret); -- Circle Seat
+
+-- SELECT * FROM Bookings;
+-- SELECT * FROM Performances WHERE Performances.PerformanceID = 1;
