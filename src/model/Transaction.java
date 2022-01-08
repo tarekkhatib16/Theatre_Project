8d83e51412f06b60734298598c13ce0e5a3fce07
StuMccly <stuartmcclymont@live.com>
2022-01-08 16:00:06 +0000

    V9_2_5

---
+++ src/model/Transaction.java
@@ -0,0 +1,35 @@
+package model;
+
+import java.util.*;
+
+public class Transaction {
+
+	/*
+	 * Instance variables including a 3D ArrayList for storing tickets.
+	 */
+	private ArrayList<ArrayList<Integer>> booking;
+
+	/*
+	 * Constructor that initialises each element of 3D ArrayList 'tickets'.
+	 */
+	public Transaction() {
+		booking = new ArrayList<ArrayList<Integer>>();
+	}
+
+	/*
+	 * Method to return 3D ArrayList tickets.
+	 */
+	public ArrayList<ArrayList<Integer>> getTickets() {
+		return booking;
+	}
+
+	/*
+	 * Method to add ticket information to 3D ArrayList 'tickets'.
+	 */
+	public void addTickets(int i, int x, int y, int z) {
+		booking.get(i).add(0, x);
+		booking.get(i).add(1, y);
+		booking.get(i).add(2, z);
+	}
+
+}
